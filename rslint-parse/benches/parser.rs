85a11d48de2c48ef45c0c9279ed9110f20d44666
RDambrosio016 <cavalier.archer@gmail.com>
2020-06-16 02:53:42 -0400

    Feat: expression benchmark

---
+++ rslint-parse/benches/parser.rs
@@ -0,0 +1,15 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use rslint_parse::parser::Parser;
+
+pub fn expr_serialization(b: &mut Criterion) {
+    let src = "a += 6 ? {\"a\": b, 7: foo} : 700 * 5 / 1 - [a, b, c]";
+
+    b.bench_function("parse expressions",
+        |b| b.iter(|| {
+            Parser::with_source(src, "bench", true).unwrap().parse_expr().unwrap();
+        })
+    );
+}
+
+criterion_group!(benches, expr_serialization);
+criterion_main!(benches);
