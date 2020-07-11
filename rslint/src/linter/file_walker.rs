8cb982befb1d711d18ed0b3e907aa6f1a5354ce6
RDambrosio016 <cavalier.archer@gmail.com>
2020-07-11 14:50:23 -0400

    Feat: parallelize file walker file loading and run rustfmt

--- rslint/src/linter/file_walker.rs
+++ rslint/src/linter/file_walker.rs
@@ -1,109 +1,144 @@
-use codespan_reporting::files::SimpleFile;
 use codespan_reporting::files::Files;
-use std::ops::Range;
-use std::collections::HashMap;
-use std::fs::File;
-use std::error::Error;
-use std::io::Read;
-use std::ffi::OsStr;
+use codespan_reporting::files::SimpleFile;
 use glob::glob;
+use std::collections::HashMap;
+use std::error::Error;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::io::Read;
+use std::ops::Range;
+use std::path::Path;
 use walkdir::WalkDir;
-
 
 /// A struct for loading and managing the files to be linted
 pub struct FileWalker {
-  pub glob: String,
-  pub files: HashMap<String, SimpleFile<String, String>>
+    pub glob: String,
+    pub files: HashMap<String, SimpleFile<String, String>>,
 }
 
 impl FileWalker {
-  pub fn new(glob: String) -> Self {
-    Self { 
-      glob,
-      files: HashMap::new(),
-    }
-  }
-
-  pub fn with_files(files: Vec<(String, String)>) -> Self {
-    let mut res = Self {
-      glob: String::new(),
-      files: HashMap::new(),
-    };
-    for file in files {
-      res.add(file.0, file.1);
-    }
-    res
-  }
-
-  pub fn load(&mut self) -> Result<(), Box<dyn Error>> {
-    let paths = glob(&self.glob)?;
-    for i in paths {
-      // Skip any unreadable files/directories
-      if i.is_err() { continue; }
-
-      for entry in WalkDir::new(i.unwrap()) {
-        // TODO: issue error if there was an error walking the dir
-        if entry.is_err() { continue }
-        let walked_entry = entry.unwrap();
-
-        let path = walked_entry.path();
-
-        // TODO: in the future the config will allow for other files to be configured to be linted
-        // this needs to be made dynamic based on config
-        if path.extension() == Some(OsStr::new("js")) {
-          let path_str = path.to_string_lossy().to_string();
-          let file = File::open(path);
-  
-          // Skip files that cannot be opened
-          if file.is_err() { continue }
-          // TODO: issue warning for unreadable files, this will currently panic 
-          let source = self.get_file_source(&mut file.unwrap()).unwrap();
-          self.add(path_str, source);
+    pub fn new(glob: String) -> Self {
+        Self {
+            glob,
+            files: HashMap::new(),
         }
-      }
     }
-    Ok(())
-  }
 
-  fn get_file_source(&self, file: &mut File) -> Result<String, Box<dyn Error>> {
-    let mut buf: Vec<u8> = vec![];
-    file.read_to_end(&mut buf)?;
-    Ok(String::from_utf8_lossy(&buf).to_string())
-  }
+    pub fn with_files(files: Vec<(String, String)>) -> Self {
+        let mut res = Self {
+            glob: String::new(),
+            files: HashMap::new(),
+        };
+        for file in files {
+            res.add(file.0, file.1);
+        }
+        res
+    }
 
-  pub fn get_existing_source(&self, key: &str) -> Option<&String> {
-    self.files.get(key).map(|file| file.source())
-  }
+    pub fn load(&mut self) -> Result<Vec<String>, Box<dyn Error>> {
+        let mut diagnostics = vec![];
+        let paths = glob(&self.glob)?;
+        let mut handles = vec![];
 
-  pub fn add(&mut self, name: String, source: String) -> usize {
-    let file_id = self.files.len();
-    self.files.insert(name.clone(), SimpleFile::new(name, source));
-    file_id
-  }
+        for i in paths {
+            // Skip any unreadable files/directories
+            if i.is_err() {
+                diagnostics.push(format!(
+                    "Skipping unreadable path at `{}`",
+                    i.unwrap_err().path().to_string_lossy()
+                ));
+                continue;
+            }
 
-  pub fn get(&self, file_id: &str) -> Option<&SimpleFile<String, String>> {
-    self.files.get(file_id)
-  }
+            for entry in WalkDir::new(i.unwrap()) {
+                // TODO: issue error if there was an error walking the dir
+                if entry.is_err() {
+                    diagnostics.push(format!(
+                        "Skipping unreadable file at `{}`",
+                        entry
+                            .unwrap_err()
+                            .path()
+                            .unwrap_or(Path::new("{unknown}"))
+                            .to_string_lossy()
+                    ));
+                    continue;
+                }
+                let walked_entry = entry.unwrap();
+
+                let path = walked_entry.path().to_owned();
+
+                // TODO: in the future the config will allow for other files to be configured to be linted
+                // this needs to be made dynamic based on config
+                if path.extension() == Some(OsStr::new("js")) {
+                    // Split up io bound operations across threads
+                    handles.push(std::thread::spawn(move || {
+                        let path_str = path.to_string_lossy().to_string();
+                        let file = File::open(path);
+
+                        // Skip files that cannot be opened
+                        if file.is_err() {
+                            Err(format!("Skipping unreadable file at `{}`", path_str))
+                        } else {
+                            let mut buf: Vec<u8> = vec![];
+                            file.unwrap().read_to_end(&mut buf).map_err(|e| format!("Encountered an error trying to read a file at `{}`: {}", path_str, e.to_string()))?;
+                            let source = String::from_utf8_lossy(&buf).to_string();
+                            Ok((path_str, source))
+                        }
+                    }));
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        for handle in handles {
+            let res = handle.join().expect("Failed to join a thread handle");
+            if res.is_err() {
+                diagnostics.push(res.unwrap_err());
+                continue;
+            } else {
+                let file = res.unwrap();
+                self.add(file.0, file.1);
+            }
+        }
+
+        Ok(diagnostics)
+    }
+
+    pub fn get_existing_source(&self, key: &str) -> Option<&String> {
+        self.files.get(key).map(|file| file.source())
+    }
+
+    pub fn add(&mut self, name: String, source: String) -> usize {
+        let file_id = self.files.len();
+        self.files
+            .insert(name.clone(), SimpleFile::new(name, source));
+        file_id
+    }
+
+    pub fn get(&self, file_id: &str) -> Option<&SimpleFile<String, String>> {
+        self.files.get(file_id)
+    }
 }
 
 impl<'a> Files<'a> for FileWalker {
-  type FileId = &'a str;
-  type Name = String;
-  type Source = String;
+    type FileId = &'a str;
+    type Name = String;
+    type Source = String;
 
-  fn name(&self, file_id: &'a str) -> Option<Self::Name> {
-    Some(self.get(file_id)?.name().clone())
-  }
+    fn name(&self, file_id: &'a str) -> Option<Self::Name> {
+        Some(self.get(file_id)?.name().clone())
+    }
 
-  fn source(&self, file_id: &'a str) -> Option<Self::Source> {
-      Some(String::from(self.get(file_id)?.source()))
-  }
+    fn source(&self, file_id: &'a str) -> Option<Self::Source> {
+        Some(String::from(self.get(file_id)?.source()))
+    }
 
-  fn line_index(&self, file_id: &'a str, byte_index: usize) -> Option<usize> {
-      self.get(file_id)?.line_index((), byte_index)
-  }
+    fn line_index(&self, file_id: &'a str, byte_index: usize) -> Option<usize> {
+        self.get(file_id)?.line_index((), byte_index)
+    }
 
-  fn line_range(&self, file_id: &'a str, line_index: usize) -> Option<Range<usize>> {
-      self.get(file_id)?.line_range((), line_index)
-  }
+    fn line_range(&self, file_id: &'a str, line_index: usize) -> Option<Range<usize>> {
+        self.get(file_id)?.line_range((), line_index)
+    }
 }
--- rslint/src/linter/mod.rs
+++ rslint/src/linter/mod.rs
@@ -1,108 +1,92 @@
 pub mod file_walker;
 
-use rslint_parse::{lexer::lexer::Lexer, diagnostic::ParserDiagnostic};
-use rslint_parse::parser::Parser;
-use std::error::Error;
 use crate::linter::file_walker::FileWalker;
-use rayon::prelude::*;
+use codespan_reporting::diagnostic::Diagnostic;
+use codespan_reporting::diagnostic::Severity;
+use codespan_reporting::term;
+use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
+use codespan_reporting::term::DisplayStyle;
 use rayon::iter::ParallelIterator;
+use rayon::prelude::*;
+use rslint_parse::parser::Parser;
+use rslint_parse::{diagnostic::ParserDiagnostic, lexer::lexer::Lexer};
+use std::error::Error;
 use std::io;
 
 pub struct Linter {
-  walker: FileWalker,
+    walker: FileWalker,
 }
 
 impl Linter {
-  pub fn new(target: String) -> Self {
-    Self {
-      walker: FileWalker::new(target),
-    }
-  }
-
-  pub fn repl() {
-    loop {
-      let mut source = String::new();
-
-      io::stdin().read_line(&mut source)
-        .expect("Failed to read line");
-      
-      let linter = Self {
-        walker: FileWalker::with_files(vec![(String::from("REPL.js"), source)])
-      };
-
-      let lexer = Lexer::new(linter.walker.get_existing_source("REPL.js").unwrap(), "REPL.js");
-      linter.lexer_debug(lexer);
-
-      print!("\n > ");
-    }
-  }
-
-  pub fn with_source(source: String, filename: String) -> Self {
-    Self {
-      walker: FileWalker::with_files(vec![(filename, source)])
-    }
-  }
-
-  pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
-    self.walker.load()?;
-    self.walker.files.par_iter().for_each(|file| {
-      let mut parser = Parser::with_source(file.1.source(), file.0, true).unwrap();
-      parser.parse_stmt_list(None, None);
-      for i in parser.errors {
-        self.throw_diagnostic(i);
-      }
-    });
-    Ok(())
-  }
-
-  fn lexer_debug(&self, lexer: Lexer) {
-    use rslint_parse::lexer::token::TokenType::Whitespace;
-
-    println!("\n Lexer debug for: {} ------------------- \n", lexer.file_id);
-    use ansi_term::Style;
-    use ansi_term::Color::*;
-
-    let source = lexer.source;
-    let mut cur_ln = 1;
-    for token in lexer {
-      if token.1.is_some() {
-        self.throw_diagnostic(token.1.unwrap());
-      }
-      if token.0.is_none() { break }
-      let tok = token.0.unwrap();
-      if tok.line > cur_ln {
-        println!();
-        cur_ln += 1;
-      }
-      if tok.token_type == Whitespace {
-        print!("{}", Style::new().on(White).paint(" ".repeat(tok.lexeme.size())));
-        continue;
-      }
-      print!(" {}({})", Style::new().on(Cyan).fg(Black).paint(format!("{:?}", tok.token_type)).to_string(), tok.lexeme.content(source));
-    }
-  }
-
-  fn throw_diagnostic(&self, diagnostic: ParserDiagnostic) {
-    use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
-    use codespan_reporting::term::DisplayStyle;
-    use codespan_reporting::term;
-    use codespan_reporting::diagnostic::Severity;
-
-    let writer = if diagnostic.diagnostic.severity == Severity::Error {
-      StandardStream::stderr(ColorChoice::Always)
-    } else {
-      StandardStream::stdout(ColorChoice::Always)
-    };
-
-    let mut config = term::Config::default();
-    config.chars.multi_top_left = '┌';
-    config.chars.multi_bottom_left = '└';
-    config.chars.source_border_left_break = '┼';
-    if diagnostic.simple {
-      config.display_style = DisplayStyle::Short;
+    pub fn new(target: String) -> Self {
+        Self {
+            walker: FileWalker::new(target),
+        }
     }
 
-    term::emit(&mut writer.lock(), &config, &self.walker, &diagnostic.diagnostic)
-      .expect("Failed to throw diagnostic");
-  }
+    pub fn repl() {
+        loop {
+            let mut source = String::new();
+
+            io::stdin()
+                .read_line(&mut source)
+                .expect("Failed to read line");
+
+            let linter = Self {
+                walker: FileWalker::with_files(vec![(String::from("REPL.js"), source)]),
+            };
+
+            let lexer = Lexer::new(
+                linter.walker.get_existing_source("REPL.js").unwrap(),
+                "REPL.js",
+            );
+            unimplemented!();
+
+            print!("\n > ");
+        }
+    }
+
+    pub fn with_source(source: String, filename: String) -> Self {
+        Self {
+            walker: FileWalker::with_files(vec![(filename, source)]),
+        }
+    }
+
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        // Throw warnings for errors while loading files
+        self.walker.load()?.iter().for_each(|diagnostic| self.diagnostic(Severity::Warning, diagnostic));
+        self.walker.files.par_iter().for_each(|file| {
+            unimplemented!();
+        });
+        Ok(())
+    }
+
+    pub fn diagnostic(&self, severity: Severity, msg: &str) {
+      let diagnostic = Diagnostic::new(severity).with_message(msg);
+      self.throw_diagnostic(diagnostic, false);
+    }
+
+    fn throw_diagnostic(&self, diagnostic: Diagnostic<&str>, short: bool) {
+        let writer = if diagnostic.severity == Severity::Error {
+            StandardStream::stderr(ColorChoice::Always)
+        } else {
+            StandardStream::stdout(ColorChoice::Always)
+        };
+
+        let mut config = term::Config::default();
+        config.chars.multi_top_left = '┌';
+        config.chars.multi_bottom_left = '└';
+        config.chars.source_border_left_break = '┼';
+        if short {
+            config.display_style = DisplayStyle::Short;
+        }
+
+        term::emit(
+            &mut writer.lock(),
+            &config,
+            &self.walker,
+            &diagnostic,
+        )
+        .expect("Failed to throw diagnostic");
+    }
 }
