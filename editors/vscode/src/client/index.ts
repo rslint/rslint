03d6462bbbdd3b0d74cc2290c7bc7f5654e3dba5
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-10-05 17:20:38 -0600

    Define vscode client in editors/vscode

---
+++ editors/vscode/src/client/index.ts
@@ -0,0 +1,43 @@
+import * as lspClient from "vscode-languageclient";
+import * as vscode from "vscode";
+
+export async function launch(context: vscode.ExtensionContext): Promise<lspClient.LanguageClient> {
+  const run: lspClient.Executable = {
+    command: "rslint-lsp",
+  };
+  const debug: lspClient.Executable = {
+    command: "rslint-lsp",
+    options: {
+      env: {
+        RUST_BACKTRACE: 1,
+        RUST_LOG: "info",
+        ...process.env,
+      },
+    },
+  };
+  const serverOptions: lspClient.ServerOptions = { debug, run };
+  const clientOptions: lspClient.LanguageClientOptions = {
+    diagnosticCollectionName: "rslint-lsp",
+    documentSelector: [
+      { language: "javascript", scheme: "file" },
+      { language: "javascript", scheme: "untitled" },
+    ],
+    synchronize: {
+      fileEvents: [
+        vscode.workspace.createFileSystemWatcher("**/*.js"),
+        vscode.workspace.createFileSystemWatcher("**/*.mjs"),
+      ],
+    },
+    middleware: {} as lspClient.Middleware,
+  };
+  const languageClient = new lspClient.LanguageClient(
+    "rslint-lsp",
+    "RSLint Language Server",
+    serverOptions,
+    clientOptions,
+  );
+  const session = languageClient.start();
+  context.subscriptions.push(session);
+  await languageClient.onReady();
+  return languageClient;
+}
