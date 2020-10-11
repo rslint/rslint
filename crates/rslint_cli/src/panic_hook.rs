80786b3ac174b419e4d16011724d41830bf4d831
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-10-10 21:42:24 -0700

    Locking the colorful stderr

--- crates/rslint_cli/src/panic_hook.rs
+++ crates/rslint_cli/src/panic_hook.rs
@@ -1,16 +1,22 @@
 //! The custom panic hook used by the linter to issue a more descriptive explanation.
 
 use crate::{lint_err, lint_note};
-use std::io::{stderr, Write};
+use codespan_reporting::term::termcolor::{self, ColorChoice};
+use std::io::Write;
 use std::panic::PanicInfo;
 
 pub fn panic_hook(info: &PanicInfo) {
     lint_err!("The linter panicked unexpectedly. this is a bug.");
 
-    let stderr = stderr();
+    let stderr = termcolor::StandardStream::stderr(ColorChoice::Always);
+
     let mut stderr_lock = stderr.lock();
 
-    writeln!(stderr_lock, "We would appreciate a bug report: https://github.com/RDambrosio016/RSLint/issues/new?labels=ILE%2C+bug&template=internal-linter-error.md\n").expect("panic_hook failed to write to stderr");
+    let mut write = |msg: &str| {
+        writeln!(stderr_lock, "{}", msg).expect("panic_hook failed to write to stderr");
+    };
+
+    write("We would appreciate a bug report: https://github.com/RDambrosio016/RSLint/issues/new?labels=ILE%2C+bug&template=internal-linter-error.md\n");
 
     lint_note!("Please include the following info: ");
 
@@ -25,7 +31,7 @@ pub fn panic_hook(info: &PanicInfo) {
         .map(|l| format!("{}", l))
         .unwrap_or_default();
 
-    writeln!(stderr_lock, "message: {}", msg).expect("panic_hook failed to write to stderr");
-    writeln!(stderr_lock, "location: {}", location).expect("panic_hook failed to write to stderr");
+    write(format!("message: {}", msg).as_str());
+    write(format!("location: {}", location).as_str());
     std::process::exit(-1);
 }
