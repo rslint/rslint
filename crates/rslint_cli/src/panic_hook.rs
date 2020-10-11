fbb014ef4b94380df56029148d7548d03225bfae
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-10-10 22:03:05 -0700

    Changed the two lint macros into writes

--- crates/rslint_cli/src/panic_hook.rs
+++ crates/rslint_cli/src/panic_hook.rs
@@ -1,13 +1,10 @@
 //! The custom panic hook used by the linter to issue a more descriptive explanation.
 
-use crate::{lint_err, lint_note};
 use codespan_reporting::term::termcolor::{self, ColorChoice};
 use std::io::Write;
 use std::panic::PanicInfo;
 
 pub fn panic_hook(info: &PanicInfo) {
-    lint_err!("The linter panicked unexpectedly. this is a bug.");
-
     let stderr = termcolor::StandardStream::stderr(ColorChoice::Always);
 
     let mut stderr_lock = stderr.lock();
@@ -16,9 +13,11 @@ pub fn panic_hook(info: &PanicInfo) {
         writeln!(stderr_lock, "{}", msg).expect("panic_hook failed to write to stderr");
     };
 
+    write("The linter panicked unexpectedly. This is a bug.");
+
     write("We would appreciate a bug report: https://github.com/RDambrosio016/RSLint/issues/new?labels=ILE%2C+bug&template=internal-linter-error.md\n");
 
-    lint_note!("Please include the following info: ");
+    write("Please include the following info: ");
 
     let msg = info
         .payload()
