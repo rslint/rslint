b0552581541ca33149464fc8ce5e3628802588b7
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-10-10 20:59:13 -0700

    Lock stderr before writing to it
    This is a basic lock on stderr in the panic_hook.

--- crates/rslint_cli/src/panic_hook.rs
+++ crates/rslint_cli/src/panic_hook.rs
@@ -2,10 +2,16 @@
 
 use crate::{lint_err, lint_note};
 use std::panic::PanicInfo;
+use std::io::{stderr, Write};
 
 pub fn panic_hook(info: &PanicInfo) {
     lint_err!("The linter panicked unexpectedly. this is a bug.");
-    eprintln!("We would appreciate a bug report: https://github.com/RDambrosio016/RSLint/issues/new?labels=ILE%2C+bug&template=internal-linter-error.md\n");
+
+    let stderr = stderr();
+    let mut stderr_lock = stderr.lock();
+
+    writeln!(stderr_lock, "We would appreciate a bug report: https://github.com/RDambrosio016/RSLint/issues/new?labels=ILE%2C+bug&template=internal-linter-error.md\n").expect("panic_hook failed to write to stderr");
+
     lint_note!("Please include the following info: ");
 
     let msg = info
@@ -13,11 +19,11 @@ pub fn panic_hook(info: &PanicInfo) {
         .downcast_ref::<String>()
         .map(|x| x.to_string())
         .unwrap_or_default();
-    eprintln!("message: {}", msg);
-    eprintln!(
-        "location: {}",
-        info.location()
+
+    let location = info.location()
             .map(|l| format!("{}", l))
-            .unwrap_or_default()
-    );
+            .unwrap_or_default();
+
+    writeln!(stderr_lock, "message: {}", msg).expect("panic_hook failed to write to stderr");
+    writeln!(stderr_lock, "location: {}", location).expect("panic_hook failed to write to stderr");
 }
