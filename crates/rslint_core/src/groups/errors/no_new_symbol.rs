5cb204e4ccb59d1f2e56d3510af1c8a392e1d0c3
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-10-17 01:03:17 +0200

    add no-new-symbol rule

---
+++ crates/rslint_core/src/groups/errors/no_new_symbol.rs
@@ -0,0 +1,64 @@
+use crate::rule_prelude::*;
+use ast::*;
+use SyntaxKind::*;
+
+declare_lint! {
+    /**
+    `Symbol` shouldn't be constructed using `new` keyword, instead
+    it should be called as a function.
+
+    ## Incorrect code examples
+
+    ```js
+    // This call results in TypeError
+    const fooSymbol = new Symbol("foo"); 
+    ```
+
+    ## Correct code examples
+
+    ```js
+    const fooSymbol = Symbol("foo");
+    ```
+    */
+    #[derive(Default)]
+    NoNewSymbol,
+    errors,
+    "no-new-symbol",
+}
+
+#[typetag::serde]
+impl CstRule for NoNewSymbol {
+    #[allow(clippy::blocks_in_if_conditions)]
+    fn check_node(&self, node: &SyntaxNode, ctx: &mut RuleCtx) -> Option<()> {
+        if node.kind() == NEW_EXPR
+            && node.to::<NewExpr>().object()?.syntax().text() == "Symbol"
+        {
+            let err = ctx
+                .err(
+                    self.name(),
+                    "`Symbol` cannot be called as a constructor.",
+                )
+                .primary(range, "");
+
+            ctx.add_err(err);
+        }
+        None
+    }
+}
+
+rule_tests! {
+    NoNewSymbol::default(),
+    err: {
+        "
+        new Symbol()
+        ",
+    },
+    ok: {
+        "
+        Symbol()
+        ",
+        "
+        new SomeClass()
+        "
+    }
+}
