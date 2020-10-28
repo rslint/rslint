2ca79a484103fdf2e55726d8597d9b88bfdf3877
Justus K <justus.k@protonmail.com>
2020-10-28 21:24:24 +0100

    Fix: simplify solution for stop rewinding template

--- crates/rslint_parser/src/state.rs
+++ crates/rslint_parser/src/state.rs
@@ -8,8 +8,6 @@ use crate::{CompletedMarker, Parser, SyntaxKind, TokenSet};
 /// It is required for things such as strict mode or async functions
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ParserState {
-    /// `token_pos` and `event_pos`
-    pub parsed_template: bool,
     /// If false, object expressions are not allowed to be parsed
     /// inside an expression.
     ///
@@ -54,7 +52,6 @@ pub enum StrictMode {
 impl Default for ParserState {
     fn default() -> Self {
         Self {
-            parsed_template: false,
             allow_object_expr: true,
             include_in: true,
             continue_allowed: false,
@@ -75,7 +72,6 @@ impl Default for ParserState {
 impl ParserState {
     pub fn module() -> Self {
         Self {
-            parsed_template: false,
             allow_object_expr: true,
             include_in: true,
             continue_allowed: false,
