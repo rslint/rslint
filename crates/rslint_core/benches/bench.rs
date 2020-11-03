01248af61776f4e977ded9d7c3f5cff8004b2c41
Stu <justus.k@protonmail.com>
2020-11-03 21:02:28 +0100

    Chore: Add benchmarks and CI setup for publishing results (#66)
    * Feat: add parser and lexer benchmarks
    
    * Fix: download engine262 instead of vendoring
    
    * Feat: add lint benchmark and `bench` script
    
    * CI: setup benchmark workflow
    
    * Chore: add benchmark to core changelog
    
    * CI: fix ci hopefully
    
    * Ci: use fork of action and provide package option

---
+++ crates/rslint_core/benches/bench.rs
@@ -0,0 +1,39 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use rslint_core::CstRuleStore;
+use rslint_lexer::Lexer;
+use rslint_parser::parse_text;
+
+const ENGINE_262_URL: &str = "https://engine262.js.org/engine262/engine262.js";
+
+fn parse(source: &str) {
+    parse_text(source, 0);
+}
+
+fn tokenize(source: &str) {
+    Lexer::from_str(source, 0).for_each(drop);
+}
+
+fn lint(source: &str) {
+    let _ = rslint_core::lint_file(0, source, false, &CstRuleStore::new().builtins(), false);
+}
+
+fn bench_source(c: &mut Criterion, name: &str, source: &str) {
+    let mut group = c.benchmark_group(name);
+    group.sample_size(10);
+    group.throughput(Throughput::Bytes(source.len() as u64));
+    group.bench_function("tokenize", |b| b.iter(|| tokenize(black_box(&source))));
+    group.bench_function("parse", |b| b.iter(|| parse(black_box(&source))));
+    group.bench_function("lint", |b| b.iter(|| lint(black_box(&source))));
+    group.finish();
+}
+
+fn engine262(c: &mut Criterion) {
+    let source = ureq::get(ENGINE_262_URL)
+        .call()
+        .into_string()
+        .expect("failed to get engine262 source code");
+    bench_source(c, "engine262", &source);
+}
+
+criterion_group!(benches, engine262);
+criterion_main!(benches);
