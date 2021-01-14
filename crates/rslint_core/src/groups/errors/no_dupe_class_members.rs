659e6c29d4f38d789ca4c5ab65ffee47baa23e12
alidn <ali.nili128@gmail.com>
2021-01-14 15:05:46 -0800

    refactor and handle overloading in TS

--- crates/rslint_core/src/groups/errors/no_dupe_class_members.rs
+++ crates/rslint_core/src/groups/errors/no_dupe_class_members.rs
@@ -6,7 +6,7 @@ use crate::rule_prelude::*;
 
 declare_lint! {
     /**
-        Disallow defining a class method more than once.
+        Disallows defining a class method more than once, unless that method is overload in TypeScript.
 
         If there are declarations of the same name in class members, the last declaration overwrites other declarations silently.
         It can cause unexpected behaviors.
@@ -48,6 +48,16 @@ declare_lint! {
             static bar() { }
             bar() { }
         }
+
+        ```
+
+        ```ts
+        // note: this is valid because of method overloading in TypeScript
+        class Foo {
+            foo(a: string): string;
+            foo(a: number): number;
+            foo(a: any): any {}
+        }
         ```
     */
     #[derive(Default)]
@@ -60,150 +70,147 @@ declare_lint! {
 impl CstRule for NoDupeClassMembers {
     fn check_node(&self, node: &SyntaxNode, mut ctx: &mut RuleCtx) -> Option<()> {
         if node.kind() == CLASS_BODY {
-            let getters = node
-                .children()
-                .filter_map(|n| {
-                    if n.kind() == GETTER {
-                        Some(n.to::<ast::Getter>())
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-            if let Some(d) = check(self.name(), &mut ctx, getters) {
-                ctx.add_err(d);
-                return None;
-            }
+            let getters = children_of_kind::<ast::Getter>(node, GETTER);
+            check_class_members(self.name(), &mut ctx, getters).ok()?;
 
-            let setters = node
-                .children()
-                .filter_map(|n| {
-                    if n.kind() == SETTER {
-                        Some(n.to::<ast::Setter>())
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-            if let Some(d) = check(self.name(), &mut ctx, setters) {
-                ctx.add_err(d);
-                return None;
-            }
+            let setters = children_of_kind::<ast::Setter>(node, SETTER);
+            check_class_members(self.name(), &mut ctx, setters).ok()?;
 
-            let methods = node
-                .children()
-                .filter_map(|n| {
-                    if n.kind() == METHOD {
-                        Some(n.to::<ast::Method>())
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-            if let Some(d) = check(self.name(), &mut ctx, methods) {
-                ctx.add_err(d);
-                return None;
-            }
+            let methods = children_of_kind::<ast::Method>(node, METHOD);
+            check_class_members(self.name(), &mut ctx, methods).ok()?;
         }
 
         None
     }
 }
 
-fn check<T: Named>(code: &str, ctx: &mut RuleCtx, nodes: Vec<T>) -> Option<Diagnostic> {
-    let mut names = HashMap::new();
-    for n in &nodes {
-        let mut name = Named::name(n);
-        if name == "constructor" {
+/// Checks if there is a duplicate in the given class members.
+/// Adds a `no-dupe-class-member` error to the given `RuleCtx` and returns
+/// the diagnostic as Err if it finds an error. Otherwise, it returns Ok.
+fn check_class_members(
+    code: &str,
+    ctx: &mut RuleCtx,
+    members: Vec<impl ClassMember>,
+) -> Result<(), Diagnostic> {
+    let mut identities = HashMap::new();
+    for m in &members {
+        if m.is_empty_decl() {
             continue;
         }
-        if n.is_static() {
-            name = format!("static {}", name);
-        }
-        if let Some(prev_decl) = names.insert(name.clone(), n.name_node()) {
+
+        let ident = {
+            if m.is_static() {
+                format!("static {}", m.identity())
+            } else {
+                m.identity()
+            }
+        };
+
+        if let Some(prev_decl) = identities.insert(ident.clone(), m.name()) {
             let err = ctx
-                .err(code, format!("Duplicate name '{}'", name))
+                .err(code, format!("Duplicate name `{}`", m.name().to_string()))
                 .secondary(
                     prev_decl,
-                    format!("Previous declaration of the method '{}' here", &name),
+                    format!(
+                        "Previous declaration of the method `{}` here",
+                        &m.name().to_string()
+                    ),
                 )
-                .primary(n.name_node(), format!("'{}' redefined here", name));
-            return Some(err);
+                .primary(
+                    m.name(),
+                    format!("`{}` redefined here", m.name().to_string()),
+                );
+            ctx.add_err(err.clone());
+            return Err(err);
         }
     }
 
-    None
+    Ok(())
 }
 
-trait Named {
-    fn name(&self) -> String;
+fn children_of_kind<T: AstNode>(node: &SyntaxNode, syn_kind: SyntaxKind) -> Vec<T> {
+    node.children()
+        .filter_map(|n| {
+            if n.kind() == syn_kind {
+                Some(n.to::<T>())
+            } else {
+                None
+            }
+        })
+        .collect()
+}
 
-    fn name_node(&self) -> SyntaxNode;
+trait ClassMember {
+    /// Returns a unique representation of the class member. If two class members
+    /// have the same identity, they are considered duplicated.
+    fn identity(&self) -> String;
+
+    /// Returns the syntax node corresponding to the name of the member.
+    fn name(&self) -> SyntaxNode;
 
     fn is_static(&self) -> bool;
+
+    /// Returns true if the it is a method declaration without an implementation body.
+    /// For example:
+    /// ```js
+    /// class Foo {
+    ///     bar(): any;
+    /// }    
+    /// ```
+    fn is_empty_decl(&self) -> bool;
 }
 
-impl Named for ast::Getter {
-    fn name(&self) -> String {
-        self.name_node().to_string()
+impl ClassMember for ast::Getter {
+    fn identity(&self) -> String {
+        self.name().to_string()
     }
 
-    fn name_node(&self) -> SyntaxNode {
-        let names = self
-            .syntax()
-            .children()
-            .filter_map(|n| {
-                if n.kind() == NAME {
-                    Some(n.to::<ast::Name>())
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<ast::Name>>();
-        names[1].syntax().clone()
+    fn name(&self) -> SyntaxNode {
+        self.key().unwrap().syntax().clone()
     }
 
     fn is_static(&self) -> bool {
         self.syntax().first_token().unwrap().kind() == STATIC_KW
     }
+
+    fn is_empty_decl(&self) -> bool {
+        self.body().is_none()
+    }
 }
 
-impl Named for ast::Setter {
-    fn name(&self) -> String {
-        self.name_node().to_string()
+impl ClassMember for ast::Setter {
+    fn identity(&self) -> String {
+        self.name().to_string()
     }
 
-    fn name_node(&self) -> SyntaxNode {
-        let names = self
-            .syntax()
-            .children()
-            .filter_map(|n| {
-                if n.kind() == NAME {
-                    Some(n.to::<ast::Name>())
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<ast::Name>>();
-        names[1].syntax().clone()
+    fn name(&self) -> SyntaxNode {
+        self.key().unwrap().syntax().clone()
     }
 
     fn is_static(&self) -> bool {
         self.syntax().first_token().unwrap().kind() == STATIC_KW
     }
+
+    fn is_empty_decl(&self) -> bool {
+        self.body().is_none()
+    }
 }
 
-impl Named for ast::Method {
-    fn name(&self) -> String {
+impl ClassMember for ast::Method {
+    fn identity(&self) -> String {
         self.name().unwrap().as_string().unwrap()
     }
 
-    fn name_node(&self) -> SyntaxNode {
+    fn name(&self) -> SyntaxNode {
         self.name().unwrap().syntax().clone()
     }
 
     fn is_static(&self) -> bool {
         self.syntax().first_token().unwrap().kind() == STATIC_KW
+    }
+
+    fn is_empty_decl(&self) -> bool {
+        self.body().is_none()
     }
 }
 
@@ -226,5 +233,10 @@ rule_tests! {
         "class A { [12]() {} [123]() {} }",
         "class A { [0x1]() {} [`0x1`]() {} }",
         "class A { [null]() {} ['']() {} }",
+        "class Foo {
+            foo(a: string): string;
+            foo(a: number): number;
+            foo(a: any): any {}
+          }"
     }
 }
