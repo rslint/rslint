24032391b6bcea24d8a9ded4bdde976048d9f07f
sorsini <sorsini@wayfair.com>
2021-10-11 11:26:57 -0400

    Simplify with try_to

--- crates/rslint_core/src/groups/errors/require_yield.rs
+++ crates/rslint_core/src/groups/errors/require_yield.rs
@@ -40,39 +40,36 @@ declare_lint! {
 #[typetag::serde]
 impl CstRule for RequireYield {
   fn check_node(&self, node: &SyntaxNode, context: &mut RuleCtx) -> Option<()> {
-      if node.kind() == SyntaxKind::FN_DECL  {
-        // if the function doesn't have a star token, it is not a generator
-        let fn_decl_node = node.to::<FnDecl>();
-        if fn_decl_node.star_token().is_some() {
+    let fn_decl = node.try_to::<FnDecl>()?;
 
-           // if the fn body is empty, return early
-           if fn_decl_node.body()?.stmts().count() == 0 {
-               return None
-           }
+    // if the function doesn't have a star token, it is not a generator
+    if fn_decl.star_token().is_some() {
 
-           let yield_expr = fn_decl_node.body()?.stmts()
-              // get all the expressions in the function block stmts
-              .filter_map(|x| match x {
-                Stmt::ExprStmt(expr) => expr.expr(),
-                _ => None,
-              })
-              // find a yield expression if one exists
-              .find(|x| matches!(x, Expr::YieldExpr(_)));
+       // if the fn body is empty, return early
+       if fn_decl.body()?.stmts().count() == 0 {
+           return None
+       }
 
-            // if none of the statements in the fn block
-            // were a yield statement, report the lint
-            if yield_expr.is_none() {
-                let err = context.err(self.name(), "This generator function does not have 'yield'.")
-                  .primary(node.trimmed_range(), "Add a 'yield' statement.");
-                context.add_err(err);
-            }
+       let yield_expr = fn_decl.body()?.stmts()
+          // get all the expressions in the function block stmts
+          .filter_map(|x| match x {
+            Stmt::ExprStmt(expr) => expr.expr(),
+            _ => None,
+          })
+          // find a yield expression if one exists
+          .find(|x| matches!(x, Expr::YieldExpr(_)));
+
+        // if none of the statements in the fn block
+        // were a yield statement, report the lint
+        if yield_expr.is_none() {
+            let err = context.err(self.name(), "This generator function does not have 'yield'.")
+              .primary(node.trimmed_range(), "Add a 'yield' statement.");
+            context.add_err(err);
         }
-      }
+    }
     None
   }
-
 }
-
 
 
 rule_tests! {
